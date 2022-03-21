051862659e4f4a621fddbb9f25f3c141a145a365
johyeri <johello62@gmail.com>
2022-03-21 15:55:15 +0900

    MODIFY App.js & package.json

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
+  Route
 } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
-  return <Router> 
-    <Switch>
-      <Route path="/movie/:id">
-        <Detail />
-      </Route>
-      <Route path="/">
-        <Home />
-      </Route>
-    </Switch>
-  </Router>;
+  return (
+    <Router> 
+      <Switch>
+        <Route path="/movie/:id" element={<Detail />} />
+        <Route path="/" element={<Home />}  />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -5,6 +5,7 @@ function Detail() {
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
   const [movieInfo, setMovieInfo] = useState([]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const getMovie = async () => {
     const json = await (//await는 async 함수 내부에 있지 않으면 사용할 수 없다
     await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
@@ -13,7 +14,7 @@ function Detail() {
   };
   useEffect(() => {
     getMovie();
-  }, []);
+  }, [getMovie]);
   return (
     <div>
       {loading ? (
@@ -23,11 +24,6 @@ function Detail() {
            <img src={movieInfo.medium_cover_image} alt={movieInfo.title}/>
            <h2>{movieInfo.title_long}</h2>
            <p>{movieInfo.description_intro}</p>
-           <ul>
-             {movieInfo.genres.map((g) => (
-               <li key={g}>{g}</li>
-             ))}
-           </ul>
          </div>
        )}
      </div>
